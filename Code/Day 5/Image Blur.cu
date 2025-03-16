9e0b2f05798d5a03d51209e7f30075e7872da38d
Majd Al-Morawed <majdmorawed@gmail.com>
2025-03-16 07:52:27 +0400

    update code

--- Code/Day 5/Image Blur.cu
+++ Code/Day 5/Image Blur.cu
@@ -1,32 +1,43 @@
 #include<iostream>
 #include<cuda_runtime.h>
+#include<time.h>
+#include<stdlib.h>
 
 __global__ void imageBlurKernel(float *out, float *in, int height, int width, int blurSize)
 {
     int row = blockIdx.y * blockDim.y + threadIdx.y;
     int col = blockIdx.x * blockDim.x + threadIdx.x;
 
+    //printf("thread (%d,%d) of block (%d,%d)",threadIdx.y,threadIdx.x,blockIdx.y,blockIdx.x);
     if(row < height && col < width)
     {
         int pixels = 0;
         float sum = 0;
+        // printf("row %d:\t", row);
+        // printf("col %d:\n", col);
 
         int linearizedIndex = row*width + col;
 
-        for(int i = -blurSize; i < blurSize + 1; i++)
+        for(int i = -blurSize; i < (blurSize + 1); ++i)
         {
-            row = row + i;
-            for(int j = -blurSize; j < blurSize + 1; j++)
+            for(int j = -blurSize; j < (blurSize + 1); ++j)
             {
-                col = col + j;
-                if(row >= 0 && row < M && col >= 0 && col < N)
+                int blurRow = row + i;
+                int blurCol = col + j;
+                // printf("blurRow: %d\t", blurRow);
+                // printf("blurCol: %d\n", blurCol);
+                if(blurRow >= 0 && blurRow < height && blurCol >= 0 && blurCol < width)
                 {
-                    sum += in[row*width + col];
-                    pixels++;
+                    sum += in[blurRow*width + blurCol];
+                    pixels += 1;
+                    // printf("sum: %f\t", sum);
+                    // printf("pixels: %f\n", pixels);
                 }
+
             }
         }
         out[linearizedIndex] = sum / pixels;
+        // printf("out = %f\n",out[linearizedIndex]);
     }
 }
 
@@ -34,17 +45,16 @@ int main()
 {
     int height = 10;
     int width  = 10;
-    int blur = 3;
-    int size = height*width * sizeof(float);
+    int sizeArray = height*width;
+    int blur = 1;
+    int size = height * width * sizeof(int);
 
-    float *A_h = new float[height][width];
-    float *B_h = new float[height][width];
+    float *A_h = new float[sizeArray];
+    float *B_h = new float[sizeArray];
 
-    srand(time(NULL));
-
-    for(int i = 0; i < height*width; i++)
+    for(int i = 0; i < (height*width); i++)
     {
-        A[i] = rand();
+        A_h[i] = rand() % 101;
     }
 
     printf("A = \n");
@@ -52,21 +62,22 @@ int main()
     {
         for(int j = 0; j < width; j++)
         {
-            printf("%f",A_h[i]);
+            printf("%f\t",A_h[i * width + j]);
         }
         printf("\n");
     }
 
-    cudaMalloc((void **)&A, size);
-    cudaMalloc((void **)&A, size);
-
     float *A_d, *B_d;
+    cudaMalloc((void **)&A_d, size);
+    cudaMalloc((void **)&B_d, size);
+    
     cudaMemcpy(A_d, A_h, size, cudaMemcpyHostToDevice);
+    cudaMemcpy(B_d, B_h, size, cudaMemcpyHostToDevice);
 
     dim3 dimGrid(2, 2, 1);
     dim3 dimBlock(5, 5, 1);
 
-    imageBlurKernel<<dimGrid, dimBlock>>(B_d, A_d, height, width, blur);
+    imageBlurKernel<<<dimGrid, dimBlock>>>(B_d, A_d, height, width, blur);
 
     cudaMemcpy(B_h, B_d, size, cudaMemcpyDeviceToHost);
 
@@ -75,10 +86,11 @@ int main()
     {
         for(int j = 0; j < width; j++)
         {
-            printf("%f",B_d[i]);
+            printf("%f\t",B_h[i * width + j]);
         }
         printf("\n");
     }
-
+    cudaFree(A_d);
+    cudaFree(B_d);
 
 }
